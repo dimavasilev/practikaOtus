6bcfd52666c7aa2d284ab56f47e96e971e7b6384
DimaSnowe <likgooo@mail.ru>
2024-12-21 20:19:11 +0300

    Добавил UI

---
+++ src/test/java/extensions/UiExtensions.java
@@ -0,0 +1,18 @@
+package extensions;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class UiExtensions implements BeforeEachCallback, AfterEachCallback {
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+
+    }
+}
--- src/test/java/otus/TecherBlock_Test.java
+++ src/test/java/otus/TecherBlock_Test.java
@@ -1,4 +1,14 @@
 package otus;
 
+import org.junit.jupiter.api.Test;
+
+
 public class TecherBlock_Test {
+
+    @Test
+    public void clickCreateTeacherButton() {
+
+    }
+
+
 }
